3c03da8a48b3956e00f6e7018f8e44f34c8ace6e
Domenico Violi <dome.violi90@gmail.com>
2026-02-23 02:01:57 +0100

    CIG-5
    Update JwtConfig.java

--- Cignalottu-be/src/main/java/it/portfolio/violihate/Cignalottu/config/JwtConfig.java
+++ Cignalottu-be/src/main/java/it/portfolio/violihate/Cignalottu/config/JwtConfig.java
@@ -1,4 +1,20 @@
 package it.portfolio.violihate.cignalottu.config;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+@SuppressWarnings("SpringBootApplicationProperties")
 public class JwtConfig {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.access-token.expiration-ms}")
+    private long accessTokenExpirationMs;
+
+    @Value("${jwt.refresh-token.expiration-ms}")
+    private long refreshTokenExpirationMs;
+
 }
