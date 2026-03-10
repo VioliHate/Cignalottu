43fe03a3b95c0103ba791c9509224b2f077158f4
Domenico Violi <dome.violi90@gmail.com>
2026-03-10 11:14:10 +0100

    update
    header and style css

--- cignalottu-fe/src/app/components/header/header.ts
+++ cignalottu-fe/src/app/components/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
-    RouterLinkActive
+    NgOptimizedImage
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
