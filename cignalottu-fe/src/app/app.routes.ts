91511c7227e1f48decd373ff6bc667615852d341
Domenico Violi <dome.violi90@gmail.com>
2026-04-27 17:03:25 +0200

    Update app.routes.ts

--- cignalottu-fe/src/app/app.routes.ts
+++ cignalottu-fe/src/app/app.routes.ts
@@ -1,9 +1,4 @@
-import {Routes} from '@angular/router';
-import {LoginPage} from './pages/login-page/login-page';
+import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
 
-export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginPage,
-  }
-];
+export const routes: Routes = [{}];
