ffcd2b7a091a0646f829d2767b802960043ae332
Domenico Violi <dome.violi90@gmail.com>
2026-04-28 18:35:33 +0200

    Update app-dialog.ts

--- cignalottu-fe/src/app/components/app-dialog/app-dialog.ts
+++ cignalottu-fe/src/app/components/app-dialog/app-dialog.ts
@@ -1,10 +1,9 @@
-import { Component, ComponentRef, Inject, inject, Optional } from '@angular/core';
+import { Component, inject, Injector } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentPortal, PortalModule } from '@angular/cdk/portal';
-import { DialogConfig } from '../../utils/dialog/dialog-config';
 import { DialogRef } from '../../utils/dialog/dialog-ref';
 import { DIALOG_DATA } from '../../utils/dialog/dialog-tokens';
-import { LucideAngularModule, LucideIconData, Mail, User, XIcon } from 'lucide-angular';
+import { LucideAngularModule, Mail, XIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-dialog',
@@ -15,10 +14,16 @@ import { LucideAngularModule, LucideIconData, Mail, User, XIcon } from 'lucide-a
 export class AppDialog {
   protected readonly xIcon = XIcon;
   protected readonly mailIcon = Mail;
+  private injector = inject(Injector);
+
   config = inject(DIALOG_DATA);
   private dialogRef = inject(DialogRef);
 
   portal = new ComponentPortal(this.config.component);
+
+  constructor() {
+    this.portal = new ComponentPortal(this.config.component, null, this.injector);
+  }
 
   close() {
     this.dialogRef.close();
