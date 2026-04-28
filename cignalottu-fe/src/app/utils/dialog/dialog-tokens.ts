3331dec75000a55a72490c1a1e1f29dc6142d03e
Domenico Violi <dome.violi90@gmail.com>
2026-04-28 18:53:51 +0200

    Update dialog-tokens.ts

--- cignalottu-fe/src/app/utils/dialog/dialog-tokens.ts
+++ cignalottu-fe/src/app/utils/dialog/dialog-tokens.ts
@@ -2,3 +2,4 @@ import { InjectionToken } from '@angular/core';
 import { DialogConfig } from './dialog-config';
 
 export const DIALOG_DATA = new InjectionToken<DialogConfig>('DIALOG_DATA');
+export const DIALOG_REF = new InjectionToken<any>('DIALOG_REF');
