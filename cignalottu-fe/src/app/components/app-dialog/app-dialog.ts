1f33cffbfb4a7effb122374081b80a73e4c5a06c
Domenico Violi <dome.violi90@gmail.com>
2026-04-28 18:26:31 +0200

    Update app-dialog.ts

--- cignalottu-fe/src/app/components/app-dialog/app-dialog.ts
+++ cignalottu-fe/src/app/components/app-dialog/app-dialog.ts
@@ -14,38 +14,13 @@ import { LucideAngularModule, LucideIconData, Mail, User, XIcon } from 'lucide-a
 })
 export class AppDialog {
   protected readonly xIcon = XIcon;
-  portal: ComponentPortal<any>;
-  dialogRef = inject(DialogRef);
-
-  constructor(@Optional() @Inject(DIALOG_DATA) public config: DialogConfig) {
-    if (!config?.component) {
-      throw new Error('Devi passare un componente da mostrare!');
-    }
-
-    this.portal = new ComponentPortal(config.component);
-  }
-
-  close(result?: any) {
-    console.log('4. DialogRef sta chiudendo. Result:', result);
-    console.log("5. L'istanza di DialogRef è:", this);
-    this.dialogRef.close(result);
-  }
-
-  onAttached(ref: any) {
-    console.log('Componente attaccato:', ref);
-
-    if (ref instanceof ComponentRef) {
-      if (ref.instance.formSubmit) {
-        ref.instance.formSubmit.subscribe((data: any) => {
-          console.log('2. Dati arrivati a AppDialog:', data);
-          console.log("3. L'istanza di DialogRef che sto per chiudere è:", this.dialogRef);
-          this.dialogRef.close(data);
-        });
-      } else {
-        console.error('ERRORE: formSubmit non trovato sul componente!');
-      }
-    }
-  }
-
   protected readonly mailIcon = Mail;
+  config = inject(DIALOG_DATA);
+  private dialogRef = inject(DialogRef);
+
+  portal = new ComponentPortal(this.config.component);
+
+  close() {
+    this.dialogRef.close();
+  }
 }
