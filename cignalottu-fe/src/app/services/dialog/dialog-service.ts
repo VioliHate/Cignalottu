f135940731e17e806a217ac00213e54f66426ebe
Domenico Violi <dome.violi90@gmail.com>
2026-04-28 18:26:28 +0200

    Update dialog-service.ts

--- cignalottu-fe/src/app/services/dialog/dialog-service.ts
+++ cignalottu-fe/src/app/services/dialog/dialog-service.ts
@@ -1,56 +1,50 @@
-import {EnvironmentInjector, inject, Injectable, Injector} from '@angular/core';
-import {DialogConfig} from '../../utils/dialog/dialog-config';
-import {AppDialog} from '../../components/app-dialog/app-dialog';
-import {Overlay} from '@angular/cdk/overlay';
-import {ComponentPortal} from '@angular/cdk/portal';
-import {DialogRef} from '../../utils/dialog/dialog-ref';
-import {DIALOG_DATA} from '../../utils/dialog/dialog-tokens';
+import { EnvironmentInjector, inject, Injectable, Injector } from '@angular/core';
+import { DialogConfig } from '../../utils/dialog/dialog-config';
+import { AppDialog } from '../../components/app-dialog/app-dialog';
+import { Overlay } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { DialogRef } from '../../utils/dialog/dialog-ref';
+import { DIALOG_DATA } from '../../utils/dialog/dialog-tokens';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DialogService {
-
   private overlay = inject(Overlay);
   private injector = inject(EnvironmentInjector);
 
   open<T>(config: DialogConfig<T>) {
-
     const overlayRef = this.overlay.create({
       hasBackdrop: true,
       backdropClass: 'bg-black/50',
-      positionStrategy: this.overlay.position()
-        .global()
-        .centerHorizontally()
-        .centerVertically()
+      positionStrategy: this.overlay.position().global().centerHorizontally().centerVertically(),
     });
-
     const dialogRef = new DialogRef(overlayRef);
     const containerPortal = new ComponentPortal(
       AppDialog,
       null,
-      this.createInjector(config, dialogRef)
+      this.createInjector(config, dialogRef),
     );
     const containerRef = overlayRef.attach(containerPortal);
     containerRef.instance.portal = new ComponentPortal(
       config.component,
       null,
-      this.createInjector(config, dialogRef)
+      this.createInjector(config, dialogRef),
     );
     if (!config.disableClose) {
       overlayRef.backdropClick().subscribe(() => dialogRef.close());
     }
+
     return dialogRef;
   }
+
   private createInjector(data: unknown, dialogRef: DialogRef) {
     return Injector.create({
       providers: [
         { provide: DIALOG_DATA, useValue: data },
-        { provide: DialogRef, useValue: dialogRef }
+        { provide: DialogRef, useValue: dialogRef },
       ],
-      parent: this.injector
+      parent: this.injector,
     });
   }
-
-
 }
