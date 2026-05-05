397119c162a91e09275c108299fe93c6a0e69e10
Domenico Violi <dome.violi90@gmail.com>
2026-05-05 18:03:44 +0200

    Update filter-bar.ts

--- cignalottu-fe/src/app/components/filter-bar/filter-bar.ts
+++ cignalottu-fe/src/app/components/filter-bar/filter-bar.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-bar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter-bar.html',
   styleUrl: './filter-bar.css',
 })
 export class FilterBar {
+  categories = input.required<string[]>();
+  activeCategory = input.required<string>();
 
+  categoryChanged = output<string>();
+
+  selectCategory(cat: string) {
+    this.categoryChanged.emit(cat);
+  }
 }
