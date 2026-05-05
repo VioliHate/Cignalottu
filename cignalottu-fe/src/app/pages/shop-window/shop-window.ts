4db015785b86db9ab5ae10c5d2deb7ecdcfd4df5
Domenico Violi <dome.violi90@gmail.com>
2026-05-05 19:20:13 +0200

    Update shop-window.ts

--- cignalottu-fe/src/app/pages/shop-window/shop-window.ts
+++ cignalottu-fe/src/app/pages/shop-window/shop-window.ts
@@ -4,19 +4,18 @@ import { ProductCard } from '../../components/product-card/product-card';
 import { ProductCategory } from '../../model/ProductCategory';
 import { Product } from '../../model/Product';
 import { MOCK_PRODUCTS } from '../../mock/MOCK_PRODUCTS';
+import { FilterBar } from '../../components/filter-bar/filter-bar';
 
 @Component({
   selector: 'app-shop-window',
   standalone: true,
-  imports: [CommonModule, ProductCard],
+  imports: [CommonModule, ProductCard, FilterBar],
   templateUrl: './shop-window.html',
   styleUrl: './shop-window.css',
 })
 export class ShopWindow {
-  readonly categories = Object.values(ProductCategory);
-
+  readonly allCategories = Object.values(ProductCategory);
   selectedCategory = signal<ProductCategory>(ProductCategory.ALL);
-
   products = signal<Product[]>(MOCK_PRODUCTS);
 
   setFilter(cat: ProductCategory) {
@@ -24,8 +23,15 @@ export class ShopWindow {
   }
 
   filteredProducts = computed(() => {
-    const active = this.selectedCategory();
-    if (active === ProductCategory.ALL) return this.products();
-    return this.products().filter((p) => p.category === active);
+    const activeCat = this.selectedCategory();
+    const allProducts = this.products();
+    if (activeCat === ProductCategory.ALL) {
+      return allProducts;
+    }
+    return allProducts.filter((p) => p.category === activeCat);
   });
+
+  updateFilter(newCategory: ProductCategory) {
+    this.selectedCategory.set(newCategory);
+  }
 }
