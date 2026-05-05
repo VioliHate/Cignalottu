c34df4cbc21a5675594df22120877c48a15a378a
Domenico Violi <dome.violi90@gmail.com>
2026-05-05 19:09:57 +0200

    Update shop-window.ts

--- cignalottu-fe/src/app/pages/shop-window/shop-window.ts
+++ cignalottu-fe/src/app/pages/shop-window/shop-window.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+import { ProductCard } from '../../components/product-card/product-card';
+import { ProductCategory } from '../../model/ProductCategory';
+import { Product } from '../../model/Product';
+import { MOCK_PRODUCTS } from '../../mock/MOCK_PRODUCTS';
 
 @Component({
   selector: 'app-shop-window',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ProductCard],
   templateUrl: './shop-window.html',
   styleUrl: './shop-window.css',
 })
 export class ShopWindow {
+  readonly categories = Object.values(ProductCategory);
 
+  selectedCategory = signal<ProductCategory>(ProductCategory.ALL);
+
+  products = signal<Product[]>(MOCK_PRODUCTS);
+
+  setFilter(cat: ProductCategory) {
+    this.selectedCategory.set(cat);
+  }
+
+  filteredProducts = computed(() => {
+    const active = this.selectedCategory();
+    if (active === ProductCategory.ALL) return this.products();
+    return this.products().filter((p) => p.category === active);
+  });
 }
