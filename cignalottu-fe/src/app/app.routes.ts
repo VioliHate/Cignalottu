87d7278434164c1961203ba2d5a57c6f8ecf4295
Domenico Violi <dome.violi90@gmail.com>
2026-05-05 18:02:28 +0200

    Update app.routes.ts

--- cignalottu-fe/src/app/app.routes.ts
+++ cignalottu-fe/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { ShopWindow } from './pages/shop-window/shop-window';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/dashboard',
+    redirectTo: '/shop',
     pathMatch: 'full',
+  },
+  {
+    path: 'shop',
+    component: ShopWindow,
   },
 ];
