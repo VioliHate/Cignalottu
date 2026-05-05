7b0012ad86651645932f6a670c5d08a604a1f1bd
Domenico Violi <dome.violi90@gmail.com>
2026-05-05 19:09:53 +0200

    Update product-card.ts

--- cignalottu-fe/src/app/components/product-card/product-card.ts
+++ cignalottu-fe/src/app/components/product-card/product-card.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { Product } from '../../model/Product';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
 export class ProductCard {
+  product = input.required<Product>();
 
+  get stars() {
+    return Array(5)
+      .fill(0)
+      .map((_, i) => i < this.product().rating);
+  }
 }
