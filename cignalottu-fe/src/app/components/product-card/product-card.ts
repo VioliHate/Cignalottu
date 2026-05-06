a31f0761da150b81f978ebda6e9dd550b8fe5dc0
Domenico Violi <dome.violi90@gmail.com>
2026-05-06 10:40:13 +0200

    Update product-card.ts

--- cignalottu-fe/src/app/components/product-card/product-card.ts
+++ cignalottu-fe/src/app/components/product-card/product-card.ts
@@ -11,9 +11,22 @@ import { Product } from '../../model/Product';
 export class ProductCard {
   product = input.required<Product>();
 
-  get stars() {
-    return Array(5)
-      .fill(0)
-      .map((_, i) => i < this.product().rating);
+  readonly starRange = [1, 2, 3, 4, 5];
+
+  getStarType(index: number): 'full' | 'half' | 'empty' {
+    const rating = this.product().rating;
+
+    if (rating >= index) {
+      return 'full';
+    }
+    const decimalPart = rating % 1;
+    const integralPart = Math.floor(rating);
+
+    if (index === integralPart + 1) {
+      if (decimalPart >= 0.8) return 'full';
+      if (decimalPart >= 0.3) return 'half';
+    }
+
+    return 'empty';
   }
 }
